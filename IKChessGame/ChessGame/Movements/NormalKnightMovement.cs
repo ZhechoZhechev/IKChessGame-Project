3a79c867c73939b9d92ff22c2abaa5bceb8a76d4
ZhechoZhechev <mishomishev024@gmail.com>
2024-05-06 15:01:18 +0300

    Knight movement class created.

---
+++ IKChessGame/ChessGame/Movements/NormalKnightMovement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ChessGame.Common;
+using ChessGame.Movements.Contracts;
+using ChessGame.ChessBoard.Contracts;
+using ChessGame.ChessPieces.Contracts;
+
+namespace ChessGame.Movements
+{
+    internal class NormalKnightMovement : IMovement
+    {
+        private const string KnightInvalindMove = "Knights move only in L shape pattern!";
+        public void ValidateMove(IFigure figure, IBoard board, Move move)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
