89181e672746b287fc8aeb5b507b67825883e62d
ZhechoZhechev <mishomishev024@gmail.com>
2024-05-06 15:02:39 +0300

    Knight move implemented

--- IKChessGame/ChessGame/Movements/NormalKnightMovement.cs
+++ IKChessGame/ChessGame/Movements/NormalKnightMovement.cs
@@ -9,9 +9,28 @@ namespace ChessGame.Movements
     internal class NormalKnightMovement : IMovement
     {
         private const string KnightInvalindMove = "Knights move only in L shape pattern!";
+
         public void ValidateMove(IFigure figure, IBoard board, Move move)
         {
-            throw new NotImplementedException();
+            var rowDistance = Math.Abs(move.From.Row - move.To.Row);
+            var colDistance = Math.Abs(move.From.Col - move.To.Col);
+            var color = figure.Color;
+            var to = move.To;
+            var figureAtPosition = board.GetFigureAtPosition(to);
+
+            if (figureAtPosition == null || color != figureAtPosition.Color)
+            {
+                if (rowDistance == 2 && colDistance == 1)
+                {
+                    return;
+                }
+                else if (colDistance == 2 && rowDistance == 1)
+                {
+                    return;
+                }
+            }
+
+            throw new InvalidOperationException(KnightInvalindMove);
         }
     }
 }
